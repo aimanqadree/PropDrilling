f5f4e04aeef0c978561b63e6e128ba82f5e26ee9
Aiman Qadree <aimanqadree@gmail.com>
2025-03-04 17:56:38 +0530

    PropDrilling

---
+++ PropDrilling/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { ParentComponent } from './component/Propdrilling'
+
+function App() {
+  
+  return (
+    <>
+      <ParentComponent/>
+    </>
+  )
+}
+
+export default App
